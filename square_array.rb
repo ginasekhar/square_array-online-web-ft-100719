4c0a5ee6a48b70ca23aa7aeb57df264c1998e4ac
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-07 19:47:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   squared_array = []
-  #array.each do |num|
-  #  squared_array << (num * num)
-  #end
+  array.each do |num|
+    squared_array << (num * num)
+  end
   
-  squared_array = array.collect { |num| num * num }
+  #squared_array = array.collect { |num| num * num }
+  squared_array
 end
